0cec4c7c8934d7380121a0dbafd89a778839056c
wargamer88 <m.van.denzel@home.nl>
2016-04-28 12:19:02 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ProjectContractorUnity/Library/CurrentLayout.dwlt
    #	ProjectContractorUnity/Library/assetDatabase3
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000002000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000003000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000004000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000004100000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000005100000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000006000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000006100000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000007000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000008000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000008100000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000009000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000009100000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/0000000000000000a000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/0000000000000000b000000000000000
    
    #	ProjectContractorUnity/Library/metadata/00/0000000000000000c000000000000000
    
    #	ProjectContractorUnity/Library/metadata/11/1195a1591c40e2a46a503fcde5deda68.info
    
    #	ProjectContractorUnity/Library/metadata/15/156198dd17dcdeb4c96a0dfca3735884.info
    
    #	ProjectContractorUnity/Library/metadata/25/25de38ce1212e1d4fbd4e54debd64148.info
    
    #	ProjectContractorUnity/Library/metadata/27/274e940e3932b3946ba3b6b042074727.info
    
    #	ProjectContractorUnity/Library/metadata/2d/2d4c79e1ec9c610488a892306984cc28.info
    
    #	ProjectContractorUnity/Library/metadata/40/40dc748b62c284942908a813a3725c69.info
    
    #	ProjectContractorUnity/Library/metadata/50/507de3763c23b2f4a818daa87252fe11.info
    
    #	ProjectContractorUnity/Library/metadata/5c/5c8ae52501e6a6a4a80aa5ec61f87894.info
    
    #	ProjectContractorUnity/Library/metadata/60/60605cbe987a80447aab712fda9e7952.info
    
    #	ProjectContractorUnity/Library/metadata/75/75c6747acc5e1804eaad9a30a1887d64.info
    
    #	ProjectContractorUnity/Library/metadata/7c/7c003131d5471544d985ba561e813bd0.info
    
    #	ProjectContractorUnity/Library/metadata/97/971f887c79ddebf43909921a1e0e27ab.info
    
    #	ProjectContractorUnity/Library/metadata/bc/bc9f534f5c435af49b5088d00acf632d.info
    
    #	ProjectContractorUnity/Library/metadata/c0/c01e528cda70f864eac5a924a79b7a37.info
    
    #	ProjectContractorUnity/Library/metadata/ce/ce5d49241fb4d9840985fd010ad9decc.info
    
    #	ProjectContractorUnity/Library/metadata/e6/e63feca9321edec419cd1a1bdf1cf07f.info
    #	ProjectContractorUnity/Library/shadercompiler-32bit-1.log
    #	ProjectContractorUnity/Library/shadercompiler-64bit-1.log

---
+++ ProjectContractorUnity/Assets/Scripts/CameraControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.GetComponentInParent<Destroyable>().DestroyableGO)
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+                Debug.Log("You selected the " + hit.transform.name);
+            }
+
+                //Instantiate(particle, transform.position, transform.rotation);
+        }
+    }
+}
---
+++ ProjectContractorUnity/Assets/Scripts/Destroyable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroyable : MonoBehaviour {
+
+    [SerializeField]
+    private bool _destroyable = true;
+
+    public bool DestroyableGO { get { return _destroyable; } }
+}
