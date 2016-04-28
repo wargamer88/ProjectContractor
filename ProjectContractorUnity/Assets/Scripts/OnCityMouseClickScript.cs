40065c39d0c79350fa3ce5f33f0e981b899fd505
KingdomHearts <michaelbos@live.nl>
2016-04-28 14:35:21 +0200

    Earth Rotation and zooming to minigame.

---
+++ ProjectContractorUnity/Assets/Scripts/OnCityMouseClickScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnCityMouseClickScript : MonoBehaviour {
+
+    private bool _cityIsClicked = false;
+    private Camera _camera;
+	// Use this for initialization
+	void Start () {
+	    _camera = GameObject.FindObjectOfType<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(_cityIsClicked)
+        {
+            Vector3 zooming = new Vector3(0, 0, -0.1f);
+            //_camera.transform.position = _camera.transform.position - zooming;
+            _camera.transform.position = Vector3.MoveTowards(_camera.transform.position, this.transform.position, 0.1f);
+            Debug.Log(_camera.transform.position);
+            if (_camera.transform.position.z > -5)
+            {
+                Application.LoadLevel(1);
+            }
+        }
+	}
+    /// <summary>
+    /// OnMouseDown is basicly onMouseClick.
+    /// When you press a city. A minigame is loaded.
+    /// </summary>
+    void OnMouseDown()
+    {
+        _cityIsClicked = true;
+    }
+}
---
+++ ProjectContractorUnity/Assets/Scripts/SpinningEarthScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningEarthScript : MonoBehaviour {
+
+    private float _baseAngle = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnMouseDown()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+        _baseAngle = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+        _baseAngle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        pos = Input.mousePosition - pos;
+        float posX = pos.x;
+        float posY = pos.y;
+        //transform.Rotate(new Vector3(posX, 0, posY));
+        //float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - _baseAngle;
+        //transform.rotation = Quaternion.AngleAxis(ang,Vector3.right);
+        transform.rotation = Quaternion.Euler(new Vector3(posY, posX,0));
+    }
+}
