c0ea836fd147255eca5e2856c8bf9e5148fe3d59
KingdomHearts <michaelbos@live.nl>
2016-05-09 11:57:09 +0200

    New Scene with crane

---
+++ ProjectContractorUnity/Assets/Scripts/Crane/BucketScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BucketScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        //Destroy(other.gameObject);
+        if (other.transform.parent.GetComponent<DestroyableScript>())
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
