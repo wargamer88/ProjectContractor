fe15b4c005c8e0439a1b3ea76cdef2d06c9e32d7
KingdomHearts <michaelbos@live.nl>
2016-05-12 16:45:32 +0200

    Fixed bugs in minigame 3

--- ProjectContractorUnity/Assets/Scripts/CrankScript.cs
+++ ProjectContractorUnity/Assets/Scripts/CrankScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class CrankScript : MonoBehaviour {
 
@@ -33,6 +34,17 @@ public class CrankScript : MonoBehaviour {
     private int _tempIndex = 0;
     private bool _gameFinished = false;
 
+    private int _gateAmount = 0;
+
+    private bool _canClickGate = false;
+
+    private List<int> _tempSimon;
+
+    [SerializeField]
+    private int _lifes = 3;
+
+    private bool _retry = true;
+
     // Use this for initialization
     void Start () {
         _redGate = GameObject.Find("RedGate");
@@ -44,6 +56,7 @@ public class CrankScript : MonoBehaviour {
         _startIndex = _simonIndex;
         _OpenedGatesList = new List<int>();
         _correctCube = GameObject.Find("CorrectCube");
+        _tempSimon = new List<int>();
 
     }
 	
@@ -51,77 +64,85 @@ public class CrankScript : MonoBehaviour {
 	void Update () {
         if (!_gameFinished)
         {
-            SimonSays();
             ClickOnGates();
-            CheckGates(); 
+            CheckGates();
+            SimonSays();
         }
         else
         {
-            if (Halo.enabled == true)
-            {
-                Halo.enabled = false;
-            }
+           // if (Halo.enabled == true)
+           // {
+           //     Halo.enabled = false;
+           // }
         }
     }
 
     void ClickOnGates()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (_canClickGate)
         {
-            //Raycasting the position clicked
-            RaycastHit vHit = new RaycastHit();
-            Ray vRay = _camera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(vRay, out vHit, 1000))
+            if (Input.GetMouseButtonDown(0))
             {
-                if (vHit.collider.gameObject.name == "RedGate" || vHit.collider.gameObject.name == "BlueGate" || vHit.collider.gameObject.name == "GreenGate" ||
-                    vHit.collider.gameObject.name == "YellowGate" || vHit.collider.gameObject.name == "PurpleGate")
+                //Raycasting the position clicked
+                RaycastHit vHit = new RaycastHit();
+                Ray vRay = _camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(vRay, out vHit, 1000))
                 {
-                    if (vHit.collider.gameObject.transform.position.y == -73.25f)
+                    if (vHit.collider.gameObject.name == "RedGate" || vHit.collider.gameObject.name == "BlueGate" || vHit.collider.gameObject.name == "GreenGate" ||
+                        vHit.collider.gameObject.name == "YellowGate" || vHit.collider.gameObject.name == "PurpleGate")
                     {
-                        switch (vHit.collider.gameObject.name)
+                        if (vHit.collider.gameObject.transform.position.y == 0) //-73.25f)
                         {
-                            case "RedGate":
-                                _OpenedGatesList.Add(1);
-                                break;
-                            case "BlueGate":
-                                _OpenedGatesList.Add(2);
-                                break;
-                            case "GreenGate":
-                                _OpenedGatesList.Add(3);
-                                break;
-                            case "YellowGate":
-                                _OpenedGatesList.Add(4);
-                                break;
-                            case "PurpleGate":
-                                _OpenedGatesList.Add(5);
-                                break;
-                            default:
-                                break;
+                            switch (vHit.collider.gameObject.name)
+                            {
+                                case "RedGate":
+                                    _OpenedGatesList.Add(1);
+                                    Debug.Log("RedGate");
+                                    break;
+                                case "BlueGate":
+                                    _OpenedGatesList.Add(2);
+                                    Debug.Log("BlueGate");
+                                    break;
+                                case "GreenGate":
+                                    _OpenedGatesList.Add(3);
+                                    Debug.Log("GreenGate");
+                                    break;
+                                case "YellowGate":
+                                    _OpenedGatesList.Add(4);
+                                    Debug.Log("YellowGate");
+                                    break;
+                                case "PurpleGate":
+                                    _OpenedGatesList.Add(5);
+                                    Debug.Log("PurpleGate");
+                                    break;
+                                default:
+                                    break;
+                            }
+                            vHit.collider.gameObject.transform.position = new Vector3(vHit.collider.gameObject.transform.position.x, 15 /*-61.94f*/, vHit.collider.gameObject.transform.position.z);
                         }
-                        vHit.collider.gameObject.transform.position = new Vector3(vHit.collider.gameObject.transform.position.x, -61.94f, vHit.collider.gameObject.transform.position.z);
-                    }
-                    else
-                    {
-                        vHit.collider.gameObject.transform.position = new Vector3(vHit.collider.gameObject.transform.position.x, -73.25f, vHit.collider.gameObject.transform.position.z);
-                        switch (vHit.collider.gameObject.name)
+                        else
                         {
-                            case "RedGate":
-                                _OpenedGatesList.Remove(1);
-                                break;
-                            case "BlueGate":
-                                _OpenedGatesList.Remove(2);
-                                break;
-                            case "GreenGate":
-                                _OpenedGatesList.Remove(3);
-                                break;
-                            case "YellowGate":
-                                _OpenedGatesList.Remove(4);
-                                break;
-                            case "PurpleGate":
-                                _OpenedGatesList.Remove(5);
-                                break;
-                            default:
-                                break;
+                            vHit.collider.gameObject.transform.position = new Vector3(vHit.collider.gameObject.transform.position.x, 0 /*-73.25f*/, vHit.collider.gameObject.transform.position.z);
+                            switch (vHit.collider.gameObject.name)
+                            {
+                                case "RedGate":
+                                    _OpenedGatesList.Remove(1);
+                                    break;
+                                case "BlueGate":
+                                    _OpenedGatesList.Remove(2);
+                                    break;
+                                case "GreenGate":
+                                    _OpenedGatesList.Remove(3);
+                                    break;
+                                case "YellowGate":
+                                    _OpenedGatesList.Remove(4);
+                                    break;
+                                case "PurpleGate":
+                                    _OpenedGatesList.Remove(5);
+                                    break;
+                                default:
+                                    break;
+                            }
                         }
                     }
                 }
@@ -131,23 +152,28 @@ public class CrankScript : MonoBehaviour {
 
     void CheckGates()
     {
-        if (_endIndex != 0 && _OpenedGatesList.Count > (_endIndex - _startIndex))
+        Debug.Log("gate: " + _OpenedGatesList.Count);
+        if (_endIndex != 0 && _OpenedGatesList.Count > (_endIndex - _startIndex) && _correctOrder == false)
         {
             int count = _endIndex - _startIndex;
             _tempIndex = _startIndex;
-            for (int i = 0; i < count; i++)
-            {
-                if (SimonOrder[_tempIndex] == _OpenedGatesList[i] )
+            //for (int i = 0; i < count; i++)
+            //{
+                if (_tempSimon.SequenceEqual(_OpenedGatesList) )
                 {
                     _correctOrder = true;
+                    _gateAmount = 0;
+                    _tempSimon = new List<int>();
                 }
-                else
+                else if(_gateAmount == _OpenedGatesList.Count)
                 {
                     _correctOrder = false;
-                    break;
+                    _simonIndex = 0;
+                    _gateAmount = 0;
+                    _tempSimon = new List<int>();
+                    _lifes--;
                 }
-                _tempIndex++;
-            } 
+            //} 
         }
 
         if (_correctOrder)
@@ -161,15 +187,20 @@ public class CrankScript : MonoBehaviour {
                 _endIndex = 0;
                 _OpenedGatesList = new List<int>();
                 _simonShowed = false;
-                _redGate.transform.position = new Vector3(_redGate.transform.position.x, -73.25f, _redGate.transform.position.z);
-                _blueGate.transform.position = new Vector3(_blueGate.transform.position.x, -73.25f, _blueGate.transform.position.z);
-                _greenGate.transform.position = new Vector3(_greenGate.transform.position.x, -73.25f, _greenGate.transform.position.z);
-                _yellowGate.transform.position = new Vector3(_yellowGate.transform.position.x, -73.25f, _yellowGate.transform.position.z);
-                _purpleGate.transform.position = new Vector3(_purpleGate.transform.position.x, -73.25f, _purpleGate.transform.position.z);
+                _redGate.transform.position = new Vector3(_redGate.transform.position.x,0 /*-73.25f*/, _redGate.transform.position.z);
+                _blueGate.transform.position = new Vector3(_blueGate.transform.position.x,0 /*-73.25f*/, _blueGate.transform.position.z);
+                _greenGate.transform.position = new Vector3(_greenGate.transform.position.x,0 /*-73.25f*/, _greenGate.transform.position.z);
+                _yellowGate.transform.position = new Vector3(_yellowGate.transform.position.x,0 /*-73.25f*/, _yellowGate.transform.position.z);
+                _purpleGate.transform.position = new Vector3(_purpleGate.transform.position.x,0 /*-73.25f*/, _purpleGate.transform.position.z);
                 _haloShowed = false;
                 _correctOrder = false;
                 _correctTimer = 0;
+                _canClickGate = false;
             }
+        }
+        else if(_retry)
+        {
+
         }
     }
 
@@ -187,11 +218,13 @@ public class CrankScript : MonoBehaviour {
                 if (Halo != null)
                 {
                     Halo.enabled = false;
-                } 
+                }
+                _canClickGate = true;
             }
             else
             {
                 _haloTimer++;
+                _canClickGate = false;
             }
         }
         if (!_simonShowed)
@@ -199,13 +232,19 @@ public class CrankScript : MonoBehaviour {
             if (!_haloShowed)
             {
                 int Gate = SimonOrder[_simonIndex];
-                _simonIndex++;
-                Debug.Log(SimonOrder[_simonIndex]);
                 if (SimonOrder[_simonIndex] == 6)
                 {
                     _gameFinished = true;
                     _simonIndex = 0;
                 }
+                _simonIndex++;
+                _gateAmount++;
+                Debug.Log(SimonOrder[_simonIndex]);
+                if (SimonOrder[_simonIndex] == 0)
+                {
+                    _canClickGate = true;
+                }
+
                 Halo = null;
                 switch (Gate)
                 {
@@ -213,26 +252,31 @@ public class CrankScript : MonoBehaviour {
                     case 1:
                         Halo = (Behaviour)_redGate.GetComponent("Halo");
                         Halo.enabled = true;
+                        _tempSimon.Add(1);
                         break;
                     //Blue Gate
                     case 2:
                         Halo = (Behaviour)_blueGate.GetComponent("Halo");
                         Halo.enabled = true;
+                        _tempSimon.Add(2);
                         break;
                     //Green Gate
                     case 3:
                         Halo = (Behaviour)_greenGate.GetComponent("Halo");
                         Halo.enabled = true;
+                        _tempSimon.Add(3);
                         break;
                     //Yellow Gate
                     case 4:
                         Halo = (Behaviour)_yellowGate.GetComponent("Halo");
                         Halo.enabled = true;
+                        _tempSimon.Add(4);
                         break;
                     //Purple Gate
                     case 5:
                         Halo = (Behaviour)_purpleGate.GetComponent("Halo");
                         Halo.enabled = true;
+                        _tempSimon.Add(5);
                         break;
                 }
                 _haloShowed = true;
