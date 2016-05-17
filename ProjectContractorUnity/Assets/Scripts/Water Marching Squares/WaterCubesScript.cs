1a8a5734a218caa8c2120f582b8d45a321278002
wargamer88 <m.van.denzel@home.nl>
2016-05-17 11:06:57 +0200

    commit 5/17/2013

--- ProjectContractorUnity/Assets/Scripts/Water Marching Squares/WaterCubesScript.cs
+++ ProjectContractorUnity/Assets/Scripts/Water Marching Squares/WaterCubesScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class WaterCubesScript : MonoBehaviour {
 
@@ -12,6 +13,12 @@ public class WaterCubesScript : MonoBehaviour {
     private int _amountCubesY = 1;
     [SerializeField]
     private int _amountCubesZ = 1;
+    [SerializeField]
+    private float _rotateAroundX = 0;
+    [SerializeField]
+    private float _rotateAroundY = 0;
+    [SerializeField]
+    private float _rotateAroundZ = 0;
 
     private GameObject[,,] _cubesArray;
 
@@ -33,60 +40,84 @@ public class WaterCubesScript : MonoBehaviour {
                 for (int Z = 0; Z < _amountCubesZ; Z++)
                 {
                     GameObject Cube = (GameObject)Instantiate(_cubePrefab, new Vector3(X + parentX, Y + parentY, Z + parentZ), Quaternion.identity);
+                    
                     Cube.transform.parent = this.gameObject.transform;
                     //Cube.GetComponent<Renderer>().enabled = false;
                     _cubesArray[X, Y, Z] = Cube;
                 }
             }
         }
-
-        for (int Z = 0; Z < _amountCubesZ; Z++)
+        this.gameObject.transform.Rotate(new Vector3(_rotateAroundX, _rotateAroundY, _rotateAroundZ));
+        
+        for (int X = 0; X < _amountCubesX; X++)
         {
-            _cubesArray[0, 2, Z].GetComponent<CubeScript>().AmountOfWater = 5;
+            for (int Y = 0; Y < _amountCubesY; Y++)
+            {
+                for (int Z = 0; Z < _amountCubesZ; Z++)
+                {
+                    //add cube directly under the current cube
+                    if (Y-1 > -1)
+                    {
+                        _cubesArray[X, Y, Z].GetComponent<CubeScript>().ConnectedCubes.Add(_cubesArray[X, Y - 1, Z]);
+                    }
+                    //add cube directly next to the current cube in the X direction
+                    if (X + 1 < _amountCubesX)
+                    {
+                        _cubesArray[X, Y, Z].GetComponent<CubeScript>().ConnectedCubes.Add(_cubesArray[X + 1, Y, Z]);
+                    }
+
+                    //GameObject Cube = (GameObject)Instantiate(_cubePrefab, new Vector3(X + parentX, Y + parentY, Z + parentZ), Quaternion.identity);
+                    //Cube.transform.parent = this.gameObject.transform;
+                    //Cube.GetComponent<Renderer>().enabled = false;
+                    //_cubesArray[X, Y, Z] = Cube;
+                }
+            }
         }
+
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        _secTimer += Time.deltaTime;
-        if (_secTimer > 1)
-        {
-            _secTimer = 0;
-            for (int X = _amountCubesX-1; X > 0; X--)
-            {
-                for (int Y = _amountCubesY-1; Y > 0; Y--)
-                {
-                    for (int Z = _amountCubesZ-1; Z > 0; Z--)
-                    {
-                        //check water level of current cube, if there is no water continue to next cube
-                        float water = _cubesArray[X, Y, Z].GetComponent<CubeScript>().AmountOfWater;
-                        if (water == 0) continue;
+        //_secTimer += Time.deltaTime;
+        //if (_secTimer > 1)
+        //{
+        //    _secTimer = 0;
+            //for (int X = _amountCubesX-1; X > -1; X--)
+            //{
+            //    for (int Y = _amountCubesY-1; Y > -1; Y--)
+            //    {
+            //        for (int Z = _amountCubesZ-1; Z > -1; Z--)
+            //        {
+            //            //check water level of current cube, if there is no water continue to next cube
+            //            float water = _cubesArray[X, Y, Z].GetComponent<CubeScript>().AmountOfWater;
+            //            if (water == 0) continue;
 
-                        //if there is a cube below current cube put all water in cube below
-                        if (Y - 1 > -1)
-                        {
-                            _cubesArray[X, Y, Z].GetComponent<CubeScript>().AmountOfWater = 0;
-                            _cubesArray[X, Y - 1, Z].GetComponent<CubeScript>().AmountOfWater = water;
-                            //continue;
-                        }
+            //            //if there is a cube below current cube put all water in cube below
+            //            if (Y - 1 > -1)
+            //            {
+            //                _cubesArray[X, Y, Z].GetComponent<CubeScript>().AmountOfWater = 0;
+            //                _cubesArray[X, Y - 1, Z].GetComponent<CubeScript>().AmountOfWater = water;
+            //                continue;
+            //            }
 
-                        if (X + 1 < _amountCubesX)
-                        {
-                            _cubesArray[X, Y, Z].GetComponent<CubeScript>().AmountOfWater = 0;
-                            _cubesArray[X + 1, Y, Z].GetComponent<CubeScript>().AmountOfWater = water;
-                        }
-                    }
-                }
-            }
+            //            //if (X + 1 < _amountCubesX)
+            //            //{
+            //            //    _cubesArray[X, Y, Z].GetComponent<CubeScript>().AmountOfWater = 0;
+            //            //    _cubesArray[X + 1, Y, Z].GetComponent<CubeScript>().AmountOfWater = water;
+            //            //}
+            //        }
+            //    }
+            //}
 
             //last FOR to happen, makes infinite water
             /**/
             for (int Z = 0; Z < _amountCubesZ; Z++)
             {
-                _cubesArray[0, 2, Z].GetComponent<CubeScript>().AmountOfWater = 5;
+                _cubesArray[0, _amountCubesY - 1, Z].GetComponent<CubeScript>().AmountOfWater = 5;
             }
             /**/
-        }
+        //}
 
     }
 }
