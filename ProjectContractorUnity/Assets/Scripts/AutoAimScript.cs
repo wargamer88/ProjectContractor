8e587af1f8b332132b13a90c7de72f9f967e2cf9
RGravity <rogieroh@hotmail.com>
2016-05-18 14:44:26 +0200

    Herro
    # Conflicts:
    #	ProjectContractorUnity/Library/assetDatabase3
    
    #	ProjectContractorUnity/Library/metadata/00/00000000000000004000000000000000

--- ProjectContractorUnity/Assets/Scripts/AutoAimScript.cs
+++ ProjectContractorUnity/Assets/Scripts/AutoAimScript.cs
@@ -6,6 +6,9 @@ public class AutoAimScript : MonoBehaviour {
     [SerializeField]
     private float Speed;
 
+    [SerializeField]
+    private GameObject Target;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -29,9 +32,18 @@ public class AutoAimScript : MonoBehaviour {
             bullet.transform.position = transform.position + new Vector3(0, 0.77f, -0.377f);
             bullet.AddComponent<Rigidbody>();
             bullet.GetComponent<Renderer>().material.color = Color.red;
-            //Vector3 middlePoint = Camera
-            //Vector3 velocity = GetTrajectory(1,bullet.transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition))
-            //bullet.GetComponent<Rigidbody>().AddForce(velocity);
+            Vector3 middlePoint = (Target.transform.position - bullet.transform.position) + new Vector3(0, 10000, 0);
+            Debug.Log(middlePoint);
+            Vector3 velocity = GetTrajectory(1, bullet.transform.position, Target.transform.position, middlePoint);
+            Debug.Log(velocity);
+            bullet.GetComponent<Rigidbody>().AddForce(velocity *2, ForceMode.VelocityChange);
         }
     }
 }
+
+
+//function BallisticVel(target: Transform, angle: float): Vector3 { var dir = target.position - transform.position; // get target direction var h = dir.y; // get height difference dir.y = 0; // retain only the horizontal direction var dist = dir.magnitude ; // get horizontal distance var a = angle * Mathf.Deg2Rad; // convert angle to radians dir.y = dist * Mathf.Tan(a); // set dir to the elevation angle dist += h / Mathf.Tan(a); // correct for small height differences // calculate the velocity magnitude var vel = Mathf.Sqrt(dist * Physics.gravity.magnitude / Mathf.Sin(2 * a)); return vel * dir.normalized; }
+
+//var target: Transform; var grenadePrefab: Transform;
+
+//function Update() { if (Input.MouseButtonDown(0)) { var grenade: Transform = Instantiate(grenadePrefab,...,...); grenade.rigidbody.velocity = BallisticVel(target, 45); // pass the angle and the target transform } } The higher the angle, the lesser the error when the heights are different (but the velocity is also lower, what may be a good thing for a grenade). The function doesn't check for invalid parameters, thus it may throw exceptions if the angle is too high (near to 90) or too low (near to 0).
