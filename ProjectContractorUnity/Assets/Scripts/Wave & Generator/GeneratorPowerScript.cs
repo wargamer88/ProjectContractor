d38cc5363cbf9ac21351a19c49578033d5a7f626
KingdomHearts <michaelbos@live.nl>
2016-05-19 13:28:55 +0200

    Scripts Update

--- ProjectContractorUnity/Assets/Scripts/Wave & Generator/GeneratorPowerScript.cs
+++ ProjectContractorUnity/Assets/Scripts/Wave & Generator/GeneratorPowerScript.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class GeneratorPowerScript : MonoBehaviour {
 
-    private float _generatorEngergy = 0;
+    private float _generatorEngergy = 100;
     private float _oldTime = 0;
     [SerializeField]
     private float _timer = 0;
     public float GeneratorEnergy { set { _generatorEngergy = value;} get { return _generatorEngergy; } }
 
-    private int _amount = 4;
+    private int _amount = 0;
     public int Amount { set { _amount = value; } get { return _amount; } }
     // Use this for initialization
     void Start () {
@@ -22,7 +22,11 @@ public class GeneratorPowerScript : MonoBehaviour {
         if (Time.time > (_oldTime + _timer))
         {
             _oldTime = Time.time;
-            _generatorEngergy = _generatorEngergy + _amount;
+            _generatorEngergy = _generatorEngergy - _amount;
+        }
+        if (_generatorEngergy < 50)
+        {
+
         }
     }
 }
