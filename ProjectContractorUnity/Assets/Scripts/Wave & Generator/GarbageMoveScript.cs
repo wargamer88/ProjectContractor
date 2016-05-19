0b5eab381fc71311a310f0c1165304bfa97ab02d
KingdomHearts <michaelbos@live.nl>
2016-05-19 16:05:55 +0200

    scripts

--- ProjectContractorUnity/Assets/Scripts/Wave & Generator/GarbageMoveScript.cs
+++ ProjectContractorUnity/Assets/Scripts/Wave & Generator/GarbageMoveScript.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class GarbageMoveScript : MonoBehaviour {
 
-    Rigidbody _rigidbody;
-	// Use this for initialization
-	void Start () {
+    private Rigidbody _rigidbody;
+
+    private float _wave = 0;
+
+
+    // Use this for initialization
+    void Start () {
         _rigidbody = GetComponent<Rigidbody>();
-	}
+        _wave = Random.Range(-15, 15);
+    }
 	
 	// Update is called once per frame
 	void Update () {
         //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.1f);
         //_rigidbody.velocity = new Vector3(0, 0, -5);
-        _rigidbody.AddRelativeForce(new Vector3(0, 0, -1));
-        _rigidbody.AddRelativeTorque(new Vector3(0, 0, -1));
+        _rigidbody.AddRelativeForce(new Vector3(_wave, 0, -1));
+        _rigidbody.AddRelativeTorque(new Vector3(_wave, 0, -1));
 	}
 }
