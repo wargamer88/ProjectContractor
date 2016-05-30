0365cc1d0566673781ff68de5edba06a7f01a2a5
wargamer88 <m.van.denzel@home.nl>
2016-05-30 15:25:09 +0200

    fix stuff

--- ProjectContractorUnity/Assets/Scripts/AutoAimScript.cs
+++ ProjectContractorUnity/Assets/Scripts/AutoAimScript.cs
@@ -24,7 +24,7 @@ public class AutoAimScript : MonoBehaviour
     private int _cooldown = 0;
     private bool _allowshoot = true;
     private float _DEBUGcounter = 0;
-    
+
     private Vector3 _moveTarget;
     private bool _isMoving = false;
     private Vector3 _currentPos;
@@ -60,7 +60,7 @@ public class AutoAimScript : MonoBehaviour
     private float _reloadAnimationTimer;
     private float _completeAnimationTimer;
 
-	// Use this for initialization
+    // Use this for initialization
     void Start()
     {
         _powerupsScript = FindObjectOfType<PowerupsScript>();
@@ -68,10 +68,10 @@ public class AutoAimScript : MonoBehaviour
         _depthMinePlane = GameObject.Find("DepthMinePlane");
         lineRenderer = GetComponent<LineRenderer>();
         _ballOffset = new Vector3(0, 10.19f, 1.82f);
-       // GetComponent<Animator>().Stop();
-	}
-	
-	// Update is called once per frame
+        // GetComponent<Animator>().Stop();
+    }
+
+    // Update is called once per frame
     void Update()
     {
         _input();
@@ -86,16 +86,16 @@ public class AutoAimScript : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(_moveTarget.x, transform.position.y, transform.position.z), 0.5f);
         }
-	}
+    }
 
     void AutoAim()
     {
 
         if (Input.GetMouseButton(0))
         {
-                Ray vRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray vRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             Physics.Raycast(vRay, out hit, 100);
-                }
+        }
         if (Input.GetMouseButtonUp(0))
         {
             if (hit.collider != null)
@@ -121,38 +121,21 @@ public class AutoAimScript : MonoBehaviour
                 {
                     if (Time.time > (_newShotTimer + (_shootAnimationTimer + _reloadAnimationTimer)) && _allowshoot && (_chosenBall == _balls[0]))  //_allowshoot && (_chosenBall == _balls[0]) )
                     {
-                    _allowshoot = false;
-                    //_bullet = GameObject.Instantiate(_chosenBall);
-                    _bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    _bullet.transform.localScale = new Vector3(2, 2, 2);
-                    _bullet.transform.position = transform.position + new Vector3(0.18f, 10.7f, 3.2f);
-                    _bullet.AddComponent<Rigidbody>();
-                    _bullet.GetComponent<Renderer>().material.color = Color.red;
-                    _bullet.AddComponent<BulletScript>();
-                    _bullet.GetComponent<BulletScript>().PowerupsScript = _powerupsScript;
-                    _bullet.AddComponent<BallGoingThroughWallScript>();
-                    Physics.IgnoreCollision(_bullet.GetComponent<SphereCollider>(), _aimPlane.GetComponent<MeshCollider>());
-                    _bullet.tag = "Projectile";
-                    Vector3 velocity = hit.point - _bullet.transform.position;
-                    //Debug.Log(velocity);
-                    transform.LookAt(hit.point);
-                    transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
-                    _bullet.GetComponent<Rigidbody>().AddForce(velocity, ForceMode.VelocityChange);
-                    _bullet.GetComponent<BulletScript>().ChosenBall = _chosenBall;
+                        _allowshoot = false;
                         _createBallAndShootingAnimation();
-                }
+                    }
                     else if (Time.time > (_newShotTimer + (_shootAnimationTimer + _reloadAnimationTimer)) && _isDepthCooldown == false && (_chosenBall == _balls[1]))
                     {
                         _isDepthCooldown = true;
                         _createBallAndShootingAnimation();
-            } 
+                    }
                     else if (Time.time > (_newShotTimer + (_shootAnimationTimer + _reloadAnimationTimer)) && _isFlameCooldown == false && (_chosenBall == _balls[2]))
                     {
                         _isFlameCooldown = true;
                         _createBallAndShootingAnimation();
-        }
+                    }
                 }
-            } 
+            }
         }
 
         if (!_allowshoot)
@@ -160,17 +143,17 @@ public class AutoAimScript : MonoBehaviour
             if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Finished"))
             {
                 _allowshoot = true;
-        }
+            }
             if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Shoot"))
             {
                 _shootAnimationTimer = (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
-    }
+            }
             if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Reload"))
-    {
+            {
                 _reloadAnimationTimer = (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
             }
             if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Nothing"))
-        {
+            {
                 _allowshoot = false;
             }
         }
