a64925a72ae19c13f65188c3423cf9d682806e0c
wargamer88 <m.van.denzel@home.nl>
2016-06-01 18:57:36 +0200

    sharky and whaley updated

--- ProjectContractorUnity/Assets/Scripts/Fishes/WhaleyScript.cs
+++ ProjectContractorUnity/Assets/Scripts/Fishes/WhaleyScript.cs
@@ -6,42 +6,75 @@ public class WhaleyScript : MonoBehaviour {
 
     [SerializeField]
     private float _speed = 10;
+    private bool _rising = true;
+    private bool _diving = false;
 
     private List<GameObject> _walls;
+    private List<GarbadgeDestoryScript> _garbage;
 
     private GameObject _garbageObject;
     private GarbageWaveScript _garbageWaveScript;
     public GameObject GarbageObject { get { return _garbageObject; } set { _garbageObject = value; } }
     public GarbageWaveScript GarbageWaveScript { get { return _garbageWaveScript; } set { _garbageWaveScript = value; } }
+    public List<GarbadgeDestoryScript> Garbage { get { return _garbage; } set { _garbage = value; } }
 
     // Use this for initialization
     void Start () {
-        _walls = GameObject.FindGameObjectsWithTag("LineWall").ToList();
-        foreach (GameObject wall in _walls)
-        {
-            Physics.IgnoreCollision(this.GetComponent<CapsuleCollider>(), wall.GetComponent<MeshCollider>());
-        }
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
         float step = _speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, GarbageObject.transform.position, step);
-    }
-
-    void OnCollisionEnter(Collision pOther)
-    {
-        if (pOther.gameObject == _garbageObject)
+        if (_rising)
         {
-            pOther.gameObject.GetComponent<GarbadgeDestoryScript>().HP--;
-            Destroy(this.gameObject);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(-0.6f, -1.4f, -13.8f), step);
 
-            if (pOther.gameObject.GetComponent<GarbadgeDestoryScript>().HP == 0)
+            if (transform.position == new Vector3(-0.6f, -1.4f, -13.8f))
             {
-                Destroy(this.gameObject);
-                _garbageWaveScript.DestroyedGarbage.Add(_garbageObject);
-                Destroy(_garbageObject);
+                foreach (GarbadgeDestoryScript Garbage in _garbage)
+                {
+                    Garbage.HP--;
+                    Garbage.transform.position = new Vector3(Garbage.transform.position.x, Garbage.transform.position.y, 85);
+                    if (Garbage.HP == 0)
+                    {
+                        _garbageWaveScript.DestroyedGarbage.Add(Garbage.gameObject);
+                        Destroy(Garbage.gameObject);
+                    }
+                }
+                _rising = false;
+                _diving = true;
+
             }
         }
+        else if (_diving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(-1, -165, 158), step);
+            if (transform.position == new Vector3(-1, -165, 158))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+
+
+
+
     }
+
+    //void OnCollisionEnter(Collision pOther)
+    //{
+    //    if (_garbage.Contains(pOther.gameObject))
+    //    {
+    //        pOther.gameObject.GetComponent<GarbadgeDestoryScript>().HP--;
+    //        //Destroy(this.gameObject);
+
+    //        if (pOther.gameObject.GetComponent<GarbadgeDestoryScript>().HP == 0)
+    //        {
+    //            //Destroy(this.gameObject);
+    //            _garbageWaveScript.DestroyedGarbage.Add(_garbageObject);
+    //            Destroy(_garbageObject);
+    //        }
+    //    }
+    //}
 }
