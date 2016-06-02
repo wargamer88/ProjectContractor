30f1e9fbc984a9cdf5c51e74fcc7f00bd66a91dd
KingdomHearts <michaelbos@live.nl>
2016-06-02 17:44:49 +0200

    Commit

---
+++ ProjectContractorUnity/Assets/Scripts/GridEventTiles/EventTileWrapperScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class EventTileWrapperScript {
+    int _choiceIndex = 0;
+
+    [SerializeField]
+    private _choices _choice;
+    [SerializeField]
+    private int _wave;
+
+    public _choices ChosenEvent { get { return _choice; } }
+    public int EventWave { get { return _wave; } }
+}
