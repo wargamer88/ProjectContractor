75372ba103c6c8a1e2a48b9987799b67a1c9940c
KingdomHearts <michaelbos@live.nl>
2016-06-21 13:10:59 +0200

    fake floating script

--- ProjectContractorUnity/Assets/Scripts/GridEventTiles/EventScripts/FakeFloatingScript.cs
+++ ProjectContractorUnity/Assets/Scripts/GridEventTiles/EventScripts/FakeFloatingScript.cs
@@ -4,13 +4,40 @@ using System.Collections;
 public class FakeFloatingScript : MonoBehaviour {
 
     private float _floating = 0;
+    private float _timer = 1f;
+    private float _oldTimer = 0;
+    private bool _isChanged = true;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        if (Time.timeScale == 1)
+        {
+            if (Time.time > (_oldTimer + _timer))
+            {
+                _oldTimer = Time.time;
+                if (_isChanged)
+                {
+                    _isChanged = false;
+                }
+                else if (_isChanged == false)
+                {
+                    _isChanged = true;
+                }
+            }
+            if (_isChanged)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 0.005f, this.transform.position.z);
+            }
+            else if (_isChanged == false)
+            {
+                this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 0.005f, this.transform.position.z);
+            }
+        }
 	
 	}
 }
